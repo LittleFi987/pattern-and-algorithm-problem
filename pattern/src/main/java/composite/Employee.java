4685618628496fbe423ce272d4a41f5279443a33
yechenhao <yechenhao@sokamail.com>
2018-02-05 23:48:48 +0800

    feat: composite pattern

---
+++ pattern/src/main/java/composite/Employee.java
@@ -0,0 +1,54 @@
+package composite;
+
+import java.util.List;
+
+/**
+ * Created by chenhao.ye on 05/02/2018.
+ */
+public class Employee {
+
+    private String name;
+
+    private String dept;
+
+    private Integer salary;
+
+    private List<Employee> employeeList;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
